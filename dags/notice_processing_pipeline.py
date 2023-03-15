a26dcf428fa58809d862bfef7dd35dfdcea663a4
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2023-03-15 17:17:48 +0200

    increase number of workers to 6

--- dags/notice_processing_pipeline.py
+++ dags/notice_processing_pipeline.py
@@ -7,7 +7,7 @@ from airflow.utils.trigger_rule import TriggerRule
 from dags import DEFAULT_DAG_ARGUMENTS
 from dags.dags_utils import get_dag_param, smart_xcom_push, smart_xcom_forward, smart_xcom_pull
 from dags.operators.DagBatchPipelineOperator import NoticeBatchPipelineOperator, NOTICE_IDS_KEY, \
-    EXECUTE_ONLY_ONE_STEP_KEY, START_WITH_STEP_NAME_KEY
+    EXECUTE_ONLY_ONE_STEP_KEY, START_WITH_STEP_NAME_KEY, DEFAULT_NUMBER_OF_CELERY_WORKERS
 from dags.pipelines.notice_batch_processor_pipelines import notices_batch_distillation_pipeline, \
     transform_notices_by_ids
 from dags.pipelines.notice_processor_pipelines import notice_normalisation_pipeline, notice_transformation_pipeline, \
@@ -47,8 +47,8 @@ def branch_selector(result_branch: str, xcom_forward_keys: List[str] = [NOTICE_I
 
 @dag(default_args=DEFAULT_DAG_ARGUMENTS,
      schedule_interval=None,
-     max_active_runs=5,
-     max_active_tasks=5,
+     max_active_runs=DEFAULT_NUMBER_OF_CELERY_WORKERS,
+     max_active_tasks=DEFAULT_NUMBER_OF_CELERY_WORKERS,
      tags=['worker', 'pipeline'])
 def notice_processing_pipeline():
     """
