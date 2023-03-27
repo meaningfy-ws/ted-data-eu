85d7160602e1a373267e4134dc742915bd00c97f
Dumitru <d.prijilevschi@gmail.com>
2023-03-27 22:59:04 +0300

    Update conftest.py

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -63,7 +63,7 @@ def ted_data_etl_pipeline():
 
 @pytest.fixture
 def etl_pipeline_config():
-    return {"start_date": "20160106", "end_date": "20160106"}
+    return {"start_date": "20190131", "end_date": "20190131"}
 
 
 @pytest.fixture
