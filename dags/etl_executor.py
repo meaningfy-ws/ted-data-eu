503780a999a2274aeb256135a005598235c6a113
Dumitru <d.prijilevschi@gmail.com>
2023-05-24 10:06:53 +0300

    testing

--- dags/etl_executor.py
+++ dags/etl_executor.py
@@ -22,16 +22,18 @@ def init_etl_pipelines_register():
         This function is used for register all ETL pipelines inside DAG.
     :return:
     """
-    etl_pipelines_register.register(etl_pipeline_name=TDA_FREE_INDEX_NAME,
-                                    etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_FREE_INDEX_NAME))
-    etl_pipelines_register.register(etl_pipeline_name=TDA_STARTER_INDEX_NAME,
-                                    etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_STARTER_INDEX_NAME))
-    etl_pipelines_register.register(etl_pipeline_name=TDA_PREMIUM_INDEX_NAME,
-                                    etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_PREMIUM_INDEX_NAME))
-    for table_name, query_path in config.TABLE_QUERY_PATHS.items():
-        etl_pipelines_register.register(etl_pipeline_name=table_name,
-                                        etl_pipeline=PostgresETLPipeline(table_name=table_name,
-                                                                        sparql_query_path=query_path))
+    # etl_pipelines_register.register(etl_pipeline_name=TDA_FREE_INDEX_NAME,
+    #                                 etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_FREE_INDEX_NAME))
+    # etl_pipelines_register.register(etl_pipeline_name=TDA_STARTER_INDEX_NAME,
+    #                                 etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_STARTER_INDEX_NAME))
+    # etl_pipelines_register.register(etl_pipeline_name=TDA_PREMIUM_INDEX_NAME,
+    #                                 etl_pipeline=TedDataETLPipeline(business_pack_name=TDA_PREMIUM_INDEX_NAME))
+    # for table_name, query_path in config.TABLE_QUERY_PATHS.items():
+    #     etl_pipelines_register.register(etl_pipeline_name=table_name,
+    #                                     etl_pipeline=PostgresETLPipeline(table_name=table_name,
+    #                                                                     sparql_query_path=query_path))
+
+    PostgresETLPipeline(table_name="Address", sparql_query_path=config.TABLE_QUERY_PATHS['Address'])
 
 
 @dag(default_args=DEFAULT_DAG_ARGUMENTS,
